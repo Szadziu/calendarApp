25188d006761e7f5637b36869c2f1e8a73f11668
Szadziu <mcj.mikolajczak@gmail.com>
2021-09-25 22:07:13 +0200

    add styles to components and starting add fetch function for get months

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,31 @@
+import { useState, useEffect } from "react";
+
 import Main from "../Main";
-import Header from "../Header/Header";
 import Modal from "../Modal";
+
+import { URL_GET_MONTH } from "../../utils/utils";
+
 import "./style.scss";
 
 function App() {
+  const getActuallyMonth = async () => {
+    try {
+      const response = await fetch(`${URL_GET_MONTH}10/2021`);
+      const data = await response.json();
+      // console.log(data.data.days);
+      return data.data.days;
+    } catch (error) {
+      return error;
+    }
+  };
+
+  const month = getActuallyMonth();
+  console.log(month);
+  const [actuallyMonth, setActuallyMonth] = useState(month);
+  console.log(actuallyMonth);
   return (
     <div className="app-container">
-      <Header />
-      <Main />
+      <Main actuallyMonth={actuallyMonth} />
       <Modal />
     </div>
   );
