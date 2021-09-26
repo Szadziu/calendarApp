8fc846d0705a809cb1b0438f5d1b6924f918d2db
Szadziu <mcj.mikolajczak@gmail.com>
2021-09-26 16:19:06 +0200

    mocking data for try display them on calendar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,25 +8,37 @@ import { URL_GET_MONTH } from "../../utils/utils";
 import "./style.scss";
 
 function App() {
-  const getActuallyMonth = async () => {
+  const fetchData = async () => {
     try {
       const response = await fetch(`${URL_GET_MONTH}10/2021`);
       const data = await response.json();
-      // console.log(data.data.days);
-      return data.data.days;
+      return data;
     } catch (error) {
       return error;
     }
   };
 
-  const month = getActuallyMonth();
-  console.log(month);
-  const [actuallyMonth, setActuallyMonth] = useState(month);
-  console.log(actuallyMonth);
+  const getMonths = async () => {
+    const data = await fetchData();
+    setMonth(data.data.days);
+    console.log("getmonths");
+  };
+
+  useEffect(() => {
+    getMonths();
+  }, []);
+  const [month, setMonth] = useState(0);
+
   return (
     <div className="app-container">
-      <Main actuallyMonth={actuallyMonth} />
-      <Modal />
+      <Main
+        actuallyMonth={[
+          { date: "01-11-2021", dayId: 0, events: [1, 2] },
+          { date: "01-11-2021", dayId: 1, events: [1, 2] },
+          { date: "01-11-2021", dayId: 2, events: [1, 2] },
+        ]}
+      />
+      {/* {activeDay && <Modal />} */}
     </div>
   );
 }
