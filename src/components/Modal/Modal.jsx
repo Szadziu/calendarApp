2748b58c9e239ede5c2d8192b12e2996f4d96413
Szadziu <mcj.mikolajczak@gmail.com>
2021-09-28 09:50:38 +0200

    add list task component, inputs and handlers for them

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,7 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+
+import TaskList from "../TaskList";
+
 import "./style.scss";
 
 const Modal = ({ day, isDisplay }) => {
@@ -13,6 +16,9 @@ const Modal = ({ day, isDisplay }) => {
     );
   }, []);
 
+  const [titleTask, setTitleTask] = useState("");
+  const [bodyTask, setBodyTask] = useState("");
+
   return (
     <>
       <div ref={modalOverlayBackground} className="modal-overlay"></div>
@@ -20,6 +26,23 @@ const Modal = ({ day, isDisplay }) => {
         {`Wybrana data to: ${day.date} 
        Masz ${day.events.length} wydarzeń na ten dzień`}
         <button>add task</button>
+        <label htmlFor="title">Title</label>
+        <input
+          value={titleTask}
+          id="title"
+          placeholder="wpisz tytuł"
+          type="text"
+          onChange={({ target: { value } }) => setTitleTask(value)}
+        />
+        <label htmlFor="body">Body</label>
+        <input
+          value={bodyTask}
+          id="body"
+          placeholder="wpisz treść"
+          type="text"
+          onChange={({ target: { value } }) => setBodyTask(value)}
+        />
+        <TaskList />
       </div>
     </>
   );
