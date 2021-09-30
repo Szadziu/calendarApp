390b1274f386bdef1c7b240fa8e49a09b14d93c2
Szadziu <mcj.mikolajczak@gmail.com>
2021-09-30 11:09:59 +0200

    tech-refactor-App-component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,48 +1,25 @@
-import { Component } from "react";
+import { useState } from "react";
 
 import Main from "../Main";
 
 import { URL_GET_MONTH } from "../../utils/utils";
 
-class App extends Component {
-  state = {
-    actuallyMonth: [],
-    currentMonth: new Date().getMonth(),
-    currentYear: new Date().getFullYear(),
-  };
+const App = () => {
+  const [actuallyMonth, setActuallyMonth] = useState([]);
 
-  setNewMonth = (numberOfMonth) => {
-    this.setState({
-      currentMonth: numberOfMonth,
-    });
-  };
-
-  setNewYear = (numberOfYear) => {
-    this.setState({
-      currentYear: numberOfYear,
-    });
-  };
-
-  fetchMonth = (month, year) => {
+  const fetchMonth = (month, year) => {
     fetch(`${URL_GET_MONTH}${month}/${year}`)
       .then((response) => response.json())
       .then((data) => {
-        this.setState({
-          actuallyMonth: data.data.days,
-        });
+        setActuallyMonth(data.data.days);
       });
   };
 
-  render() {
-    return (
-      <div>
-        <Main
-          actuallyMonth={this.state.actuallyMonth}
-          fetchMonth={this.fetchMonth}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Main actuallyMonth={actuallyMonth} fetchMonth={fetchMonth} />
+    </div>
+  );
+};
 
 export default App;
